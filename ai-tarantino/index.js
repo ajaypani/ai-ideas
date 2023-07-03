2a3987aa04915a0245c7a8fa01e64e0f47d07597
Ajay Panigrahi <79969922+ajaypani@users.noreply.github.com>
2023-07-03 17:31:54 +0100

    new functions for title, cast , image creation

--- ai-tarantino/index.js
+++ ai-tarantino/index.js
@@ -2,7 +2,6 @@ import { process } from '/env'
 import { Configuration, OpenAIApi } from 'openai'
 
 
-const setupTextarea = document.getElementById('setup-textarea') 
 const setupInputContainer = document.getElementById('setup-input-container')
 const movieBossText = document.getElementById('movie-boss-text')
 
@@ -13,6 +12,7 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration)
 
 document.getElementById("send-btn").addEventListener("click", () => {
+   const setupTextarea = document.getElementById('setup-textarea') 
    if (setupTextarea.value) {
     const userInput = setupTextarea.value
     setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`
@@ -51,7 +51,7 @@ async function fetchBotReply(outline) {
 async function fetchSynopsis(outline) {
   const response = await openai.createCompletion({
     model: 'text-davinci-003',
-    prompt: `Generate an engaging, professional and marketable movie synopsis based on an outline
+    prompt:  `Generate an engaging, professional and marketable movie synopsis based on an outline. The synopsis should include actors names in brackets after each character. Choose actors that would be ideal for this role. 
     ###
     outline: A big-headed daredevil fighter pilot goes back to school only to be sent on a deadly mission.
     synopsis: The Top Gun Naval Fighter Weapons School is where the best of the best train to refine their elite flying skills. When hotshot fighter pilot Maverick (Tom Cruise) is sent to the school, his reckless attitude and cocky demeanor put him at odds with the other pilots, especially the cool and collected Iceman (Val Kilmer). But Maverick isn't only competing to be the top fighter pilot, he's also fighting for the attention of his beautiful flight instructor, Charlotte Blackwood (Kelly McGillis). Maverick gradually earns the respect of his instructors and peers - and also the love of Charlotte, but struggles to balance his personal and professional life. As the pilots prepare for a mission against a foreign enemy, Maverick must confront his own demons and overcome the tragedies rooted deep in his past to become the best fighter pilot and return from the mission triumphant.
@@ -61,5 +61,77 @@ async function fetchSynopsis(outline) {
     `,
     max_tokens: 700
   }) 
-  document.getElementById('output-text').innerText = response.data.choices[0].text.trim()
+  const synopsis = response.data.choices[0].text.trim()
+  document.getElementById('output-text').innerText = synopsis
+  fetchTitle(synopsis)
+  fetchStars(synopsis)
+}
+
+async function fetchTitle(synopsis) {
+  const response = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `Generate a catchy movie title for this synopsis: ${synopsis}`,
+    max_tokens: 25,
+    temperature: 0.7
+  })
+  const title = response.data.choices[0].text.trim()
+  document.getElementById('output-title').innerText = title
+  fetchImagePrompt(title, synopsis)
+}
+
+async function fetchStars(synopsis){
+  const response = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `
+      Extract movie stars in the brackets from synopsis: ${synopsis}
+      ###
+      synopsis:  Charlotte Blackwood (Kelly McGillis)
+      starring:  Kelly McGillis
+      ###
+      starring: 
+      ` ,
+ 
+    max_tokens: 30
+  })
+  document.getElementById('output-stars').innerText = response.data.choices[0].text.trim()
+}
+
+async function fetchImagePrompt(title, synopsis) {
+  const response = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: `
+        Generate an image prompt for this title: ${title}
+        ###
+        title:  Bahubali
+        synopsis:  A child from the Mahishmati kingdom is raised by tribal people and one day learns about his royal heritage, 
+        his father's bravery in battle and a mission to overthrow the incumbent ruler. 
+        It's a tale of two cousins in the Kingdom of Mahishmati, India. Balla (Rana)fights his way with cousin Baahubali(Prabhas) for the throne.
+        starring: Prabhas, Rana
+        image description: Everything is in warm theme, Golden glow.Bahubali, the powerful yet obedint, kind ruler , Holds his sword on horse.
+        Rana , the powerful but jealous of his cousin, he is holding a spearhead. background actors and Stong fort of Mahishmati, India.
+        ###
+        Image description: 
+        `,
+      max_tokens: 100,
+      temperature: 0.8
+    })
+    //const image = response.data.choices[0].text.trim()
+    //document.getElementById('output-image').innerText = image
+    fetchImageUrl(response.data.choices[0].text.trim())
+}
+
+async function fetchImageUrl(imagePrompt){
+  const response = await openai.createImage({
+    prompt: `${imagePrompt}. There should be no text in this image.`,
+    n: 1,
+    size: '256x256',
+    response_format: 'b64_json' 
+  })
+  document.getElementById('output-img-container').innerHTML = `<img src="data:image/png;base64,${response.data.data[0].b64_json}">`
+  setupInputContainer.innerHTML = `<button id="view-pitch-btn" class="view-pitch-btn">View Pitch</button>`
+  document.getElementById('view-pitch-btn').addEventListener('click', ()=>{
+    document.getElementById('setup-container').style.display = 'none'
+    document.getElementById('output-container').style.display = 'flex'
+    movieBossText.innerText = `This idea is so good I'm jealous! It's gonna make you rich for sure! Remember, I want 10% 💰`
+  })
 }
